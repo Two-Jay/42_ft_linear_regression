ffb6518f6fc0e23c0b9bd7f02869493ae3812525
Two-Jay <djeeee1272@gmail.com>
2023-08-28 17:09:11 +0900

    update

--- model/model.py
+++ model/model.py
@@ -7,7 +7,6 @@ def get_theta():
             return [float(v.strip('')) for v in file.read().split(',')]
     except:
         return [0, 0]
-
 class Linear_Regression:
     @staticmethod
     def fit(x_data, y_data, learning_rate):
@@ -17,6 +16,11 @@ class Linear_Regression:
     def predict(input_x_data):
         theta = get_theta()
         return theta[0] + (theta[1] * input_x_data) if all(i == 0 for i in theta) == False else None
+
+    @staticmethod
+    def add_to_dataset(input_mileage, predicted_price):
+        with open(f"{os.getcwd()}/resources/data.csv", 'a') as file:
+            file.write(f"{input_mileage},{predicted_price}\n")
 
     @staticmethod
     def evaluate(x_data, y_data):
