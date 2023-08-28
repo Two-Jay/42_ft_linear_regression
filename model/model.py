6c18e2fca288757066586ae32cd99ec722ae7007
Two-Jay <djeeee1272@gmail.com>
2023-08-28 14:55:26 +0900

    모델 내부에 predict 코드 완성

--- model/model.py
+++ model/model.py
@@ -1,3 +1,5 @@
+import os
+
 class Linear_Regression:
     @staticmethod
     def fit(x_data, y_data):
@@ -5,7 +7,12 @@ class Linear_Regression:
 
     @staticmethod
     def predict(input_x_data):
-        pass
+        try:
+            with open(f"{os.getcwd()}/resources/theta.csv", 'r') as file:
+                theta0, theta1 = [float(v.strip('')) for v in file.read().split(',')]
+                return theta0 + (theta1 * input_x_data)
+        except:
+            print('Error : Please train the model first')
 
     @staticmethod
     def evaluate(x_data, y_data):
