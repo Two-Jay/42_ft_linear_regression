e3d9775d04819e76d3a56a8e447a1f052852d701
Two-Jay <djeeee1272@gmail.com>
2023-08-28 15:06:06 +0900

    theta 읽어오는 모듈 분리

--- model/model.py
+++ model/model.py
@@ -1,5 +1,13 @@
 import os
 
+def get_theta():
+    try:
+        with open(f"{os.getcwd()}/resources/theta.csv", 'r') as file:
+            return [float(v.strip('')) for v in file.read().split(',')]
+    except:
+        print('Error : Please train the model first')
+        exit(1)
+
 class Linear_Regression:
     @staticmethod
     def fit(x_data, y_data):
@@ -7,12 +15,8 @@ class Linear_Regression:
 
     @staticmethod
     def predict(input_x_data):
-        try:
-            with open(f"{os.getcwd()}/resources/theta.csv", 'r') as file:
-                theta0, theta1 = [float(v.strip('')) for v in file.read().split(',')]
-                return theta0 + (theta1 * input_x_data)
-        except:
-            print('Error : Please train the model first')
+        theta0, theta1 = get_theta()
+        return theta0 + (theta1 * input_x_data)
 
     @staticmethod
     def evaluate(x_data, y_data):
