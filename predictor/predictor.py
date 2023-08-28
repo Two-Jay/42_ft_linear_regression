8878717a16b560241a961f989718a4a7c6cd3cd7
Two-Jay <djeeee1272@gmail.com>
2023-08-28 17:05:47 +0900

    update

--- predictor/predictor.py
+++ predictor/predictor.py
@@ -2,12 +2,17 @@ import sys
 import os
 sys.path.insert(0, f'{os.getcwd()}/model')
 from model import Linear_Regression
+
+err_message = {
+    'invalid_input': 'Invalid input. Please try again.',
+    'model_not_trained': 'Model is not trained yet. Please run trainer.py first.'
+}
     
 def get_input_mileage() -> float:
     try:
         return float(input('Enter mileage: '))
     except:
-        print('Invalid input. Please try again.')
+        print(err_message['invalid_input'])
         return get_input_mileage()
 
 def main():
@@ -15,7 +20,7 @@ def main():
     if prediction != None:
         print(f'Estimated price: {prediction}')
     else:
-        print('Model is not trained yet. Please run trainer.py first.')
+        print(err_message['model_not_trained'])
 
 if __name__ == '__main__':
     main()
