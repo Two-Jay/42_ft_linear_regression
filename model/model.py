fda9345cb4178b236d8fd17bcebda15e7e1118d3
Two-Jay <djeeee1272@gmail.com>
2023-08-28 16:48:36 +0900

    theta 예외처리 종료시 종료 책임을 predict로 이관

--- model/model.py
+++ model/model.py
@@ -1,21 +1,24 @@
 import os
+from mutils import gradient_descent
 
 def get_theta():
     try:
         with open(f"{os.getcwd()}/resources/theta.csv", 'r') as file:
             return [float(v.strip('')) for v in file.read().split(',')]
     except:
-        print('Error : Please train the model first')
-        exit()
+        return [0, 0]
 
 class Linear_Regression:
     @staticmethod
-    def fit(x_data, y_data):
+    def fit(x_data, y_data, learning_rate):
         pass
 
     @staticmethod
     def predict(input_x_data):
         theta0, theta1 = get_theta()
+        if theta0 == 0 and theta1 == 0:
+            print('Error : Please train the model first')
+            exit()
         return theta0 + (theta1 * input_x_data)
 
     @staticmethod
